ded9b097fca1bb07a4f8a323caf3d7eeef3a21b8
JacopoRaffi <75311297+JacopoRaffi@users.noreply.github.com>
2022-10-24 10:56:12 +0200

    Print statistics of categories

--- data_classfication.py
+++ data_classfication.py
@@ -2,7 +2,7 @@ import pandas as pd
 from datetime import datetime
 
 classes = {}
-# key = year, value = [success, failed, burned, special]
+# key = year, value = [success, failed, special]
 
 def isDust(in_out: str) -> bool:
     if (in_out != ''): #check if string is not empty
@@ -14,10 +14,29 @@ def isDust(in_out: str) -> bool:
         return False      
     return False
 
-# 0->sucess; 1->failed; 2->burned; 3->special(dust collecting or other)
+#infos := timestamp','blockId','txId','isCoinbase','fee','approxSize
+#inputs := 0 or more input semicolon';' separated where input := addrId','amount','prevTxSpending',' position_of_output_in_prevTxSpending
+#outputs := 1 or more output semicolon';' separated where output := addrId','amount',' scriptType
 def tx_category(infos, inputs, outputs):
+    addresses = set([])
+    fee = int(infos[4]) #to check if tx is dust collecting
+    tot_import = 0
 
+    for input in inputs.split(";"):
+        values = input.split(",")
+        addr = int(values[0])
+        addresses.add(addr)
+        amount = int(values[1])
+        tot_import += amount
 
+    #0->sucess; 1->failed; 2->special(dust collecting or other)    
+    if(fee == tot_import):
+        return 2
+    
+    if(len(addresses) == 1):
+        return 1
+
+    return 0
 
 def classification(file):
     for line in file:
@@ -36,10 +55,24 @@ def classification(file):
 def main():
     #intialize dict for temporal statistics
     for i in range(2010, 2022):
-        classes[i] = [0, 0, 0, 0]
+        classes[i] = [0, 0, 0]
 
     file = open("../dust_notSD.txt", 'r')
     classification(file)
+    tot_0 = 0
+    tot_1 = 0
+    tot_2 = 0
+
+    for year in classes:
+        tot_0 += classes[year][0]
+        tot_1 += classes[year][1]
+        tot_2 += classes[year][2]
+    
+    print("Tx totali in cui viene speso il dust: ", tot_0 + tot_1 + tot_2)
+    print("Attacco di successo: ", tot_0)
+    print("Attacco fallito: ", tot_1)
+    print("Transazioni speciali: ", tot_2)
+
     return 0
 
 if __name__ == "__main__":
