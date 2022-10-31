27434ceaf01af5a09ecf0c370985fb18f0b28638
JacopoRaffi <75311297+JacopoRaffi@users.noreply.github.com>
2022-10-31 12:09:17 +0100

    Delete latex.py

--- latex.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-datiIn = pd.read_csv("../data_csv/inputs.csv.xz", sep=',', header=0, compression='xz')
-datiOut = pd.read_csv("../data_csv/outputs.csv.xz", sep=',', header=0, compression='xz')
-print(datiIn.head().to_latex(index=False))
-print(datiOut.head().to_latex(index=False))
